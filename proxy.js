e1ebe6b01024bc8bab45f880b7ae21bbeb2b4b6b
Rich Jones <rich@anomos.info>
2016-09-19 17:59:25 -0400

    add proxyjs script for testing

---
+++ proxy.js
@@ -0,0 +1,17 @@
+var socks = require('socksv5');
+
+var port = 9050;
+var srv = socks.createServer(function(info, accept, deny) {
+  accept();
+  //var debug = accept(true);
+  // debug.on('data', function(data){
+  //   console.log(data);
+  //   return data;
+  //  });
+
+});
+srv.listen(port, 'localhost', function() {
+  console.log('SOCKS server listening on port ' + port);
+});
+
+srv.useAuth(socks.auth.None());
