1525095aebef45594b3c3b13f43f65a6642710a9
Rich Jones <rich@anomos.info>
2016-09-20 00:10:13 -0400

    fml

--- proxy.js
+++ proxy.js
@@ -4,11 +4,6 @@ var port = 9050;
 var srv = socks.createServer(function(info, accept, deny) {
   accept();
   console.log("accepted."); 
-  var debug = accept(true);
-  debug.on('data', function(data){
-     console.log(data);
-     return data;
-  });
 
 });
 srv.listen(port, 'localhost', function() {
